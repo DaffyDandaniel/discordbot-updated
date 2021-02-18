5e86ed3d8aac47cfbc515e2f9de7454eda94a2d2
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-18 23:38:11 +0000

    report fix 2

--- commands/report.js
+++ commands/report.js
@@ -7,21 +7,21 @@ module.exports = {
         let rMember = message.mentions.members.first()
 
         if (!rMember){
-            return message.reply("The user you attempted to mention or use in your report message does not exist.").then(m => n.delete(5000));
+            return message.reply("The user you attempted to mention or use in your report message does not exist.").then(m => m.delete({ timeout: 5000 }));
         }
 
         if (rMember.roles.cache.has('800460004683415594') || rMember.user.bot){
-            return message.reply("You cannot report this person!").then(m => n.delete(5000));
+            return message.reply("You cannot report this person!").then(m => m.delete({ timeout: 5000 }));
         }
 
         if (!args[1]){
-            return message.channel.send("Please provide a reason for the report!").then(m => n.delete(5000));
+            return message.channel.send("Please provide a reason for the report!").then(m => m.delete({ timeout: 5000 }));
         }
 
         const rChannel = message.guild.channels.cache.find(channel => channel.id === '812100946648760381');
 
         if (!channel){
-            message.channel.send("The reports channel ID specified in my code is not correct! The developers have been notified of this issue.").then(m => n.delete(5000));
+            message.channel.send("The reports channel ID specified in my code is not correct! The developers have been notified of this issue.").then(m => m.delete({ timeout: 5000 }));
             client.users.fetch('347711540700512256').then((user) => {
                 user.send('**A bug has been detected in the bot!**\n\n*Bug:* Reports channel not found when command "report" was run!\n\n Please fix this issue!')
             })
