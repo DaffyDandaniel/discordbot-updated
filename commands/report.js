48592a95d9ea3b7fb9ae8342946ff888253c7aad
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-18 23:41:24 +0000

    report.js small fix

--- commands/report.js
+++ commands/report.js
@@ -20,7 +20,7 @@ module.exports = {
 
         const rChannel = message.guild.channels.cache.find(channel => channel.id === '812100946648760381');
 
-        if (!channel){
+        if (!rChannel){
             message.channel.send("The reports channel ID specified in my code is not correct! The developers have been notified of this issue.").then(m => m.delete({ timeout: 5000 }));
             client.users.fetch('347711540700512256').then((user) => {
                 user.send('**A bug has been detected in the bot!**\n\n*Bug:* Reports channel not found when command "report" was run!\n\n Please fix this issue!')
