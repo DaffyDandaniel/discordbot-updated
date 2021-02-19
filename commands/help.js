79207c1b5c6fb301b70c63dfb1cdfc3f20181482
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-19 00:10:03 +0000

    help.js report addition

--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,7 @@ module.exports = {
     *Commands for Everyone:*
     ?apply - Shows the SMP Application form (You need JAVA minecraft to apply)
     ?ping - Does not show the real ping, but shows a cool message instead.
+    ?report <ping the person who you want to report here> <reason for report> - Reports a player and sends a message to the admin team to review the report.
 
     *Commands for Server Admin+*
     ?mute <ping player here> - Mutes the pinged player if not muted.
