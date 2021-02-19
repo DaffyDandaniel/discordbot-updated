d066c4fe26edfe29ed1504b72c0720e303eecb6c
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-19 00:24:47 +0000

    dm fix

--- commands/report.js
+++ commands/report.js
@@ -20,13 +20,12 @@ module.exports = {
             return message.channel.send("Please provide a reason for the report!").then(m => m.delete({ timeout: 5000 }));
         }
 
-        const rChannel = message.guild.channels.cache.find(channel => channel.id === '812100946648760380');
+        const rChannel = message.guild.channels.cache.find(channel => channel.id === '812100946648760381');
 
         if (!rChannel){
             message.channel.send("The reports channel ID specified in my code is not correct! The developers have been notified of this issue.").then(m => m.delete({ timeout: 5000 }));
-            client.users.fetch('347711540700512256').then((user) => {
-                user.send('**A bug has been detected in the bot!**\n\n*Bug:* Reports channel not found when command "report" was run!\n\n Please fix this issue!')
-            })
+            let dm = message.guild.users.fetch('347711540700512256')
+            dm.send('**A bug has been detected in the bot!**\n\n*Bug:* Reports channel not found when command "report" was run!\n\n Please fix this issue!')
         }
 
         const embed = new Discord.MessageEmbed()
