4b4018eabcd1e0015f3d8e64c414b47d405cba5f
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-22 22:57:10 +0000

    help.js update 2

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setColor('BLUE')
         .setTitle('Help')
-        .setDescription('**Miscellaneous:**\n?apply - Shows the application to join the ADO SMP (You require Java to apply)\n?ping - This does not show the real ping but just responds with something else instead.\n\n**Reports:**\n?report <ping user> <reason for report> - Reports the player specified for the specified reason.\n?smpstaff - Shows the staff team for the ADO SMP\n\n**Moderation:**\n?mute <ping person> - Mutes the specified member.\n?unmute <ping person> - Unmutes the specified person if muted.\n?kick <ping person> - Kicks the specified person.\n?ban <ping person> - Bans the specified person.')
+        .setDescription('**Miscellaneous:**\n?apply - Shows the application to join the ADO SMP (You require Java to apply)\n?ping - This does not show the real ping but just responds with something else instead.\n?smpstaff - Shows the staff team for the ADO SMP\n\n**Reports:**\n?report <ping user> <reason for report> - Reports the player specified for the specified reason.\n\n**Moderation:**\n?mute <ping person> - Mutes the specified member.\n?unmute <ping person> - Unmutes the specified person if muted.\n?kick <ping person> - Kicks the specified person.\n?ban <ping person> - Bans the specified person.')
         .setFooter('ADO SMP || To contact an administrator please DM any administrators online.');
 
         message.react('👍')
