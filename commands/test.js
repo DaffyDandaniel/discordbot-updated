b55b7cbd0e4757489308c17599a216ff0a1d5cba
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-24 20:05:45 +0000

    test.js test

---
+++ commands/test.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'test',
+    description: "test",
+    execute(message, args){
+        if(!args.length){
+            return message.channel.send("No args!")
+        }
+
+        return message.channel.send(args.length)
+    }
+}
