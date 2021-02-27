24d99f62e2b1f4e99e02d2f8f77db1d3d86e8930
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-27 15:04:50 +0000

    say fix

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,6 @@ module.exports = {
     name: 'say',
     description: "Make me say anything you want!",
     execute(message, args){
-        message.channel.send(`*User ${message.member} has told me to say this:*\n\n**${args}**`)
+        message.channel.send(`*User ${message.member} has told me to say this:*\n\n**${args.join(" ")}**`)
     }
 }
