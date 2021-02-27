bdfeb48e16476e739460abb9093f34d18e5b603d
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-27 12:47:23 +0000

    support changed to say

---
+++ commands/say.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'say',
+    description: "Make me say anything you want!",
+    execute(message, args){
+        message.channel.send(`*User ${message.member} has told me to say this:*\n\n**${args}**`)
+    }
+}
