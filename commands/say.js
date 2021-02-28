ef36349333dbcf4dbd8d9fb2fbd6f5b2a0ee63ac
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-02-28 11:35:54 +0000

    welcome added

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,8 @@ module.exports = {
     name: 'say',
     description: "Make me say anything you want!",
     execute(message, args){
+        if (message.deletable) message.delete();
+        
         message.channel.send(`*User ${message.member} has told me to say this:*\n\n**${args.join(" ")}**`)
     }
 }
--- main.js
+++ main.js
@@ -49,4 +49,30 @@ client.on('message', message =>{
     }
 });
 
+client.on('guildMemberAdd', guildMember => {
+    let welcomerole1 = guildMember.guild.roles.cache.find(role => role.id === '800467642930561074')
+    let welcomerole2 = guildMember.guild.roles.cache.find(role => role.id === '800467872946061312')
+    let welcomerole3 = guildMember.guild.roles.cache.find(role => role.id === '800467563846696960')
+    let welcomerole4 = guildMember.guild.roles.cache.find(role => role.id === '800467734139764766')
+    let welcomechannel = guildMember.guild.channels.cache.get('812118346271162378')
+    let ruleschannel = guildMember.guild.channels.cache.get('804125503773868092')
+    let appchannel = guildMember.guild.channels.cache.get('805015019670470666')
+    let chatchannel = guildMember.guild.channels.cache.get('800458925290815530')
+    let botchannel = guildMember.guild.channels.cache.get('801489803089936444')
+    const embed = new Discord.MessageEmbed();
+
+    guildMember.roles.add(welcomerole1)
+    guildMember.roles.add(welcomerole2)
+    guildMember.roles.add(welcomerole3)
+    guildMember.roles.add(welcomerole4)
+
+    embed.setColor('ORANGE')
+    embed.setTimestamp()
+    embed.setFooter('ADO SMP | A new member has joined!')
+    embed.setTitle('Welcome to the server!')
+    embed.setDescription(`A new member has joined the server! Please welcome <@${guildMember.user.id}> to the server!\n\n**Rules:**\nYou can find the rules in this channel: ${ruleschannel}\n\n**SMP Application:** You can apply for the SMP here: ${appchannel}\n\n**Public Chat and bot commands:**\nYou can talk to others in the server here: ${chatchannel}\nYou can use bots in the server here: ${botchannel}\n\n*We hope you enjoy your time inside the ADO SMP server!*`);
+
+    return welcomechannel.send(embed);
+});
+
 client.login(process.env.token);
