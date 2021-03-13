676fb1e1a13b834b7cb2c94d3c927eeeacba09f0
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2021-03-13 22:38:14 +0000

    update fix 2

--- commands/update.js
+++ commands/update.js
@@ -2,36 +2,36 @@ module.exports = {
     name: 'update',
     description: "Updates your Nickname to the correct nickname.",
     execute(message, args){
-        if(message.author.roles.cache.has('800460004683415594')){
+        if(message.member.roles.cache.has('800460004683415594')){
             return message.delete();
         }
 
-        if(message.author.roles.cache.has('808438330995245116')){
+        if(message.member.roles.cache.has('808438330995245116')){
             return message.delete();
         }
 
-        if(message.author.roles.cache.has('804371535246000179')){
+        if(message.member.roles.cache.has('804371535246000179')){
             return message.delete();
         }
 
-        if(message.author.roles.cache.has('800465139727990794')){
+        if(message.member.roles.cache.has('800465139727990794')){
             return message.delete();
         }
 
-        if(message.author.roles.cache.has('800473969526308895')){
+        if(message.member.roles.cache.has('800473969526308895')){
             return message.delete();
         }
 
-        if(message.author.roles.cache.has('800465357374488628')){
-            return message.author.setNickname(`MOD | ${message.author.username}`);
+        if(message.member.roles.cache.has('800465357374488628')){
+            return message.member.setNickname(`MOD | ${message.author.username}`);
         }
 
-        if(message.author.roles.cache.has('800468137870360636')){
-            return message.author.setNickname(`SB | ${message.author.username}`);
+        if(message.member.roles.cache.has('800468137870360636')){
+            return message.member.setNickname(`SB | ${message.author.username}`);
         }
 
-        if(message.author.roles.cache.has('800467563846696960')){
-            return message.author.setNickname(`MEMBER | ${message.author.username}`);
+        if(message.member.roles.cache.has('800467563846696960')){
+            return message.member.setNickname(`MEMBER | ${message.author.username}`);
         }
     }
 }
